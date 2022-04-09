863e16bbb1f36582029e570993a67a611248a5c6
Waqas Anjum <Qanjum96@gmail.com>
2022-04-09 16:03:55 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Qaz',
+    streetAddress: '101 mains st'
+}
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {
+         ...employee,
+        [key]: value
+    }
+
+}
+
+// console.log(updateEmployeeWithKeyAndValue(employee, 'Eren Yeager', 'Attack titan'))
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const copyEmployee = {...employee}
+    delete copyEmployee[key]
+    return copyEmployee
+}
+
+// console.log(deleteFromEmployeeByKey(employee))
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
